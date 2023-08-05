b8c258e8dfba7dc1808c9992da9eabd2d6bedaf1
Coli-co <awestlifeblue@gmail.com>
2023-08-05 11:24:31 +0800

    pg connection cofigure

---
+++ config/pg-connect.js
@@ -0,0 +1,26 @@
+const { Pool } = require('pg')
+require('dotenv').config()
+
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDB,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT
+})
+
+// check connection
+const pgConnect = async () => {
+  try {
+    const res = await pool.query('SELECT NOW()')
+    console.log(res.rows[0]['now'])
+    await pool.end()
+    return res
+  } catch (err) {
+    console.log('Connection err:', err)
+  }
+}
+
+pgConnect()
+
+module.exports = pool
