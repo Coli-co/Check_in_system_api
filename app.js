fb6394768a36a4074e4648669719d77f2e54011a
Coli-co <awestlifeblue@gmail.com>
2023-08-05 23:12:50 +0800

    create api of listing top 5 employees with clockin earlier for a specific date

--- app.js
+++ app.js
@@ -142,6 +142,28 @@ app.get('/employees/:date', async (req, res) => {
   }
 })
 
+// list top 5 employees with clockIn earlier for a specific date
+app.get('/employees/:date/top', async (req, res) => {
+  const { date } = req.params
+  try {
+    // assuming the date format is 'YYYY-MM-DD'
+    const query = `
+      SELECT *
+      FROM employees
+      WHERE DATE(clockIn) = $1
+      ORDER BY clockIn ASC
+      LIMIT 5;
+    `
+    const { rows } = await pool.query(query, [date])
+    if (rows.length === 0) {
+      return res.status(200).json({ data: [] })
+    }
+    return res.status(200).json({ data: rows })
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal server error' })
+  }
+})
+
 app.listen(port, () => {
   console.log(`Server is listening on http://localhost:${port}`)
 })
