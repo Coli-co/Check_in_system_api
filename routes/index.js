c5e630d9fa6cf1aa61b0acd956b5e3ef7e155120
Coli-co <awestlifeblue@gmail.com>
2023-08-06 17:49:15 +0800

    employees router set

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const employees = require('./modules/employees')
+
+router.use('/employees', employees)
 
 module.exports = router
