315b5650883bfcd55cd7ad249189fb000713d36e
Coli-co <awestlifeblue@gmail.com>
2023-08-15 10:25:31 +0800

    add homepage route

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const employees = require('./modules/employees')
+const homepage = require('./modules/homepage')
 
 router.use('/employees', employees)
+router.use('/', homepage)
 
 module.exports = router
